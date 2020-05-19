8cd53a51a1a8a5afc4b7ff65d7d413a0d17a04d0
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-05-19 15:31:29 +0900

    Updates

---
+++ precache-manifest.1b9dc6f4a7770d35dec3a8ff2570cdce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00d88f0ef64906c423868ce586fa43e",
+    "url": "/e-commerce-project-contextAPI/index.html"
+  },
+  {
+    "revision": "c9dd14a3135c05d89a02",
+    "url": "/e-commerce-project-contextAPI/static/css/main.c42da526.chunk.css"
+  },
+  {
+    "revision": "6aaa2a3034452d5a2cb0",
+    "url": "/e-commerce-project-contextAPI/static/js/2.07421125.chunk.js"
+  },
+  {
+    "revision": "c9dd14a3135c05d89a02",
+    "url": "/e-commerce-project-contextAPI/static/js/main.218b9331.chunk.js"
+  },
+  {
+    "revision": "cea5f2064876d1a32cce",
+    "url": "/e-commerce-project-contextAPI/static/js/runtime~main.453f3c57.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-project-contextAPI/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-project-contextAPI/static/media/shopping-bag.30f07071.svg"
+  }
+]);
