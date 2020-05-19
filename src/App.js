76542706b29adf5b1c77e0478029c32fb5f72fe6
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-05-19 15:17:58 +0900

    commit before deploying it with gh-page

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
             },
           });
         });
-      }
+      }   
 
       this.setState({ currentUser: userAuth });
     });
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -14,7 +14,7 @@ const CollectionPage = ({ match }) => {
   //       const { title, items } = collection;
   //       return (
   //         <div className="collection-page">
-  //           <h2 className="title">{title}</h2>
+  //           <h2 className="title">{title}</h2>   
   //           <div className="items">
   //             {items.map((item) => (
   //               <CollectionItem key={item.id} item={item} />
